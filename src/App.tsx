97cf26d226104987cfa9b6f3a159ba21b2f5c9a4
purushothama123 <purushothama.k@gyanmatrix.com>
2023-09-03 23:08:44 +0530

    added user search page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Instamainpage from "./components/mainpage/InstaMainPage";
 import Profilemainpage from "./components/userprofilepage/ProfileMainPage";
-
+import SearchUser from "./components/searchuser/SearchUser";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Instamainpage />} />
         <Route path="/userprofile" element={<Profilemainpage />} />
+        <Route path="/SearchUser" element={<SearchUser />} />
       </Routes>
     </BrowserRouter>
   );
