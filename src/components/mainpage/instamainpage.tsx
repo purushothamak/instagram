35dcc2dd5978c417217250843cf94a101cc02232
purushothama123 <purushothama.k@gyanmatrix.com>
2023-09-04 12:28:44 +0530

    added feeds,menubar&status

--- src/components/mainpage/instamainpage.tsx
+++ src/components/mainpage/instamainpage.tsx
@@ -1,11 +1,17 @@
 import Profilewidget from "../profilewidget/Profilewidget";
-import Suggestedwidget from "../usersuggestions/SuggestedWidget";
+import Suggestedwidget from "../usersuggestions/suggestedwidget";
+import InstaSideBar from "../Sidebar/InstaSideBar";
+import HeaderStories from "../Header/HeaderStories";
+import InstaFeeds from "../FeedsSection/InstaFeeds";
 
 import "../../App.css";
 
 function Instamainpage() {
   return (
     <div className="rightside-content">
+      <InstaSideBar />
+      <HeaderStories />
+      <InstaFeeds />
       <Profilewidget />
       <Suggestedwidget />
     </div>
