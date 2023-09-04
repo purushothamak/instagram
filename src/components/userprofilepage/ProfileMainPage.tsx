23d784f3e81db5402a004d51cd6713ba28ac78e6
purushothama123 <purushothama.k@gyanmatrix.com>
2023-09-04 14:59:38 +0530

    fixed homepage UI issue

--- src/components/userprofilepage/ProfileMainPage.tsx
+++ src/components/userprofilepage/ProfileMainPage.tsx
@@ -2,10 +2,12 @@ import Userprofile from "./Userprofile";
 import "./userprofile.css";
 import StatusHighlight from "./statushighlightwidget/Statushightlight";
 import PostImageList from "./postsection/PostSection";
+import InstaSideBar from "../Sidebar/InstaSideBar";
 
 function Profilemainpage() {
   return (
     <div className="user-profile-container ">
+      <InstaSideBar />
       <Userprofile />
       <StatusHighlight />
       <PostImageList />
